7bd5e464489e34252db4f5e90c52468311a754be
Joao Sousa <joaosousa.pt@gmail.com>
2019-10-13 00:08:30 +0100

    Get Day/Month/Year Functions Implemented

---
+++ AEDA_T1_G6_Part1/Date_time.cpp
@@ -0,0 +1,16 @@
+#include "Date_time.h"
+
+
+
+
+size_t Date::get_day() const {
+	return day;
+}
+
+size_t Date::get_month() const {
+	return month;
+}
+
+size_t Date::get_year() const {
+	return year;
+}
